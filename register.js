5c62608e762266d0f5c6bc85035da2b45a17e422
Alexander Shepel <al.iiieii@gmail.com>
2018-03-27 16:07:35 +0300

    Initial commit Функционал сбора access_token пользователей instagram

---
+++ register.js
@@ -0,0 +1,90 @@
+const Rx = require('rxjs/Rx');
+
+const requestPromise = require('request-promise');
+const config = require('./config');
+const {
+  httpServerObservable, httpRequestObservable, sendToLogstash,
+  fsReadFileObservable, fsWriteFileObservable,
+  logError, logInfo,
+} = require('./utils');
+
+const errorHtml = (msg) =>
+  `<html>
+<head><meta charset="UTF-8"></head>
+<body><h3>${msg}</h3></body>
+</html>`;
+
+httpServerObservable(config.httpServer)
+  .mergeMap(server => httpRequestObservable({ server }))
+  .takeUntil(Rx.Observable.fromEvent(process, 'SIGINT'))
+  .filter(({ req }) => req.url !== '/favicon.ico')
+  .mergeMap(
+    ({ req, res }) => {
+      if (req.query === undefined || req.query.code === undefined || req.query.state === undefined) {
+        res.writeHead(400);
+        res.end(errorHtml('Некорректный запрос'));
+        logError(`Некорректный запрос: ${req.url}`);
+        return Rx.Observable.empty();
+      }
+      return Rx.Observable.of({ req, res })
+        .mergeMap(
+          ({ req: { query, url }, res }) =>
+            Rx.Observable.from(
+              requestPromise.post(config.instagram.access_token_url, {
+                json: true,
+                form: {
+                  client_id: config.instagram.client_id,
+                  client_secret: config.instagram.client_secret,
+                  grant_type: 'authorization_code',
+                  redirect_uri: config.instagram.redirect_uri,
+                  code: query.code,
+                },
+              }),
+            )
+              .catch(
+                err => {
+                  logError(`Ошибка получения access_token (${url}): ${err.message}`);
+                  res.writeHead(500);
+                  res.end(errorHtml('Ошибка предоставления доступа к аккаунту Instagram, выполните повторный запрос или обратитесь к администратору'), 'utf8');
+                  return Rx.Observable.empty();
+                },
+              ),
+          ({ req: { query } }, resVal) => Object.assign({}, resVal, {
+            employee_code: query.state,
+            timestamp: new Date().getTime(),
+          }),
+        )
+        .do(data => logInfo(`Получен access_token для пользовател (${data.user.username}/${data.employee_code}): ${data.access_token}`))
+        .mergeMap(
+          data =>
+            fsWriteFileObservable(`${data.employee_code}.json`, JSON.stringify(data, null, '  '), 'utf8')
+              .catch(err => {
+                console.error(err.message);
+                return Rx.Observable.of(data);
+              }),
+          srcVal => srcVal,
+        )
+        .mergeMap(
+          data =>
+            fsReadFileObservable(`users.json`, 'utf8')
+              .map(contents => JSON.parse(contents))
+              .map(dataFromFile => [...dataFromFile, data])
+              .mergeMap(sumData => fsWriteFileObservable(`users.json`, JSON.stringify(sumData, null, '  '), 'utf8'))
+              .catch(
+                err =>
+                  fsWriteFileObservable(`${data.employee_code}.json`, JSON.stringify(data, null, '  '), 'utf8')
+                    .catch(err => {
+                      logError(err.message);
+                      return Rx.Observable.empty();
+                    }),
+              ),
+          srcVal => srcVal,
+          1,
+        )
+        .do(() => {
+          res.writeHead(302, { Location: config.uokti.url });
+          res.end();
+        })
+    }
+  )
+  .subscribe();
---
+++ utils.js
@@ -0,0 +1,45 @@
+const Rx = require('rxjs/Rx');
+const urlParse = require('url-parse');
+const http = require('http');
+const fs = require('fs');
+
+module.exports.fsReadFileObservable = Rx.Observable.bindNodeCallback(fs.readFile);
+module.exports.fsWriteFileObservable = Rx.Observable.bindNodeCallback(fs.writeFile);
+
+const log = (type, msg) => console.error(`[${new Date().toString()}] [${type}] ${msg}`);
+module.exports.logError = msg => log('ERROR', msg);
+module.exports.logInfo = msg => log('INFO', msg);
+
+module.exports.httpServerObservable = ({ host, port }) =>
+  Rx.Observable.of(0)
+    .map(() => http.createServer().listen(port, host))
+    .mergeMap(
+      server =>
+        Rx.Observable.merge(
+          Rx.Observable.fromEvent(server, 'error').mergeMap(Rx.Observable.throw),
+          Rx.Observable.fromEventPattern(
+            server.addListener.bind(server, 'listening'),
+            () => {
+              server.removeAllListeners('listening');
+              server.removeAllListeners('error');
+              server.removeAllListeners('close');
+              server.close();
+            },
+          ),
+        ),
+      srcVal => srcVal,
+    );
+
+module.exports.httpRequestObservable = ({ server }) =>
+  Rx.Observable.fromEventPattern(
+    server.addListener.bind(server, 'request'),
+    server.removeListener.bind(server, 'request'),
+    (req, res) => ({ req, res }),
+  )
+    .map(({ req, res }) => ({ req: Object.assign(req, { query: urlParse(req.url, true).query }), res }))
+    .takeUntil(Rx.Observable.fromEvent(server, 'close'));
+
+module.exports.sendToLogstash = ({ url, data }) => requestPromise.post(logstashUrl, {
+  json: true,
+  body: data,
+});
