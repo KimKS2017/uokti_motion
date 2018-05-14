cc227d94614b2ab159c6815098e25b08f3a3e74a
Alexander Shepel <al.iiieii@gmail.com>
2018-05-14 17:08:18 +0300

    Добавил расчет кармы

---
+++ carma.js
@@ -0,0 +1,53 @@
+const fs = require('fs');
+const Rx = require('rxjs/Rx');
+const csv = require('csv-parse');
+const { streamToRx } = require('rxjs-stream');
+const {
+  getClient, scrollQuery, bulkUpdate,
+} = require('./utils');
+
+const config = require('./config');
+const rxClient = getClient(config);
+
+const round = (num, signs) => Math.round(Math.pow(10, signs) * num) / Math.pow(10, signs);
+const carmaForUsersNum = (n) => Math.min(n > 3 ? round(0.0179 * Math.exp(0.3413 * n), 2) : 0, 0.5);
+const carmaForUsersCarmaDiff = (d) => carmaForUsersNum(d);
+const arrayMaxDiff = (arr) => Math.max(...arr) - Math.min(...arr);
+
+const usersCurrentCarma = {};
+
+const users$ = streamToRx(fs.createReadStream('./users.csv').pipe(csv({ columns: ['login', 'displayName'] })))
+    .reduce((acc, curr) => Object.assign(acc, { [curr['login']]: curr['displayName'] }), {});
+
+const carma$ = scrollQuery({ rxClient })({
+  index: 'metrics-instagram',
+  scroll: '1m',
+  size: 100,
+  sort: "@timestamp:asc",
+})
+  .pluck('_source')
+  .withLatestFrom(users$)
+  .map(([{'@timestamp': timestamp, tagged_users}, registered_users]) => ({
+    timestamp,
+    tagged_users,
+    registered_users,
+    users: tagged_users.filter(userid => !!registered_users[userid]),
+  }))
+  .concatMap(({timestamp, users, registered_users}) => {
+    const carmas = users.map(userid => usersCurrentCarma[userid] || 0);
+    const photoCarma = round(1 + carmaForUsersNum(users.length) + carmaForUsersCarmaDiff(arrayMaxDiff(carmas)), 2);
+    users.forEach(userid => usersCurrentCarma[userid] = round((usersCurrentCarma[userid] || 0) + photoCarma, 2));
+    return Rx.Observable.from(users.map(userid => ({
+      _index: 'metrics-instagram-users',
+      _type: 'user',
+      _id: userid + timestamp,
+      _target: {
+        '@timestamp': timestamp,
+        login: userid,
+        carma: usersCurrentCarma[userid],
+        displayName: registered_users[userid]
+      }
+    })));
+  })
+  .let(bulkUpdate({rxClient, test: false, upsert: true}))
+  .subscribe(console.log);
--- utils.js
+++ utils.js
@@ -3,13 +3,18 @@ const urlParse = require('url-parse');
 const http = require('http');
 const fs = require('fs');
 const requestPromise = require('request-promise');
+const {URL} = require('url');
+const elasticsearch = require('elasticsearch');
+const RxClient = require('rx-elasticsearch');
+const _ = require('lodash');
 
 module.exports.fsReadFileObservable = Rx.Observable.bindNodeCallback(fs.readFile);
 module.exports.fsWriteFileObservable = Rx.Observable.bindNodeCallback(fs.writeFile);
 
+
 const log = (type, msg) => console.error(`[${new Date().toString()}] [${type}] ${msg}`);
-module.exports.logError = msg => log('ERROR', msg);
-module.exports.logInfo = msg => log('INFO', msg);
+const logError = msg => log('ERROR', msg);
+const logInfo = msg => log('INFO', msg);
 
 module.exports.httpServerObservable = ({ host, port }) =>
   Rx.Observable.of(0)
@@ -44,3 +49,102 @@ module.exports.sendToLogstash = (url) => (data) => requestPromise.post(url, {
   json: true,
   body: data,
 });
+
+module.exports.getClient = (config) => {
+  let esUrl = new URL(config.elasticsearch.url);
+
+  const client = new elasticsearch.Client({
+    host: [{
+      protocol: esUrl.protocol,
+      host: esUrl.hostname,
+      port: esUrl.port || (esUrl.protocol === 'https:' ? 443 : (esUrl.protocol === 'http:' ? 80 : 9200)),
+      path: esUrl.pathname,
+      query: esUrl.search,
+      auth: `${config.elasticsearch.username}:${config.elasticsearch.password}`,
+    }],
+    log: config.loglevel
+  });
+
+// Fix elasticsearch bug: https://github.com/elastic/elasticsearch-js/issues/537
+  const oldClearScroll = client.clearScroll;
+  client.clearScroll = params => oldClearScroll.call(client, Object.assign(params, {scrollId: [].concat(params.scrollId)}));
+
+  return new RxClient.default(client);
+};
+
+module.exports.scrollQuery = ({rxClient}) => (params) =>
+  rxClient
+    .scroll(Object.assign({scroll: '1m', size: 100}, params))
+    .concatMap(res => Rx.Observable.from(res.hits.hits));
+
+const bulk = ({rxClient, bulkSize = 100, test = true, concurrency = 1, retries = 3, delay = 0}) => (observable, fn) =>
+  observable
+    .concatMap(fn)
+    .bufferCount(bulkSize)
+    .mergeMap(res =>
+        Rx.Observable.of(_.flatten(res))
+          .mergeMap(res =>
+            test ? res : new Promise(
+              (resolve, reject) =>
+                rxClient.client.bulk(
+                  {body: res},
+                  (err, res) =>
+                    err ? reject({message: 'Error while executing bulk request', payload: err}) : (
+                      res.errors ? reject({
+                        message: 'Error while executing bulk request',
+                        payload: res
+                      }) : resolve({message: `${res.items.length} records processed`, payload: res})
+                    )
+                )
+            )
+          )
+          .retry(retries)
+          .delay(delay),
+      (srcVal, resVal) => resVal,
+      concurrency
+    );
+
+module.exports.bulkUpdate = ({rxClient, bulkSize, test, upsert = false, fresh = false, fresh_old_id = '_id', concurrency, retries, delay}) => (observable) =>
+  bulk({rxClient, bulkSize, test, concurrency, retries, delay})(
+    observable,
+    res =>
+      res._script
+        ? Rx.Observable.from([
+          {update: {_index: res._index, _type: res._type, _id: res._id}},
+          {script: {inline: res._script, lang: "painless"}}
+        ])
+        : (
+          res._target && _.keys(res._target).length > 0
+            ? (
+              fresh
+                ? Rx.Observable.from([
+                  [
+                    {delete: {_index: res._index, _type: res._type, _id: res[fresh_old_id]}}
+                  ],
+                  [
+                    {create: {_index: res._index, _type: res._type, _id: res._id}},
+                    res._target,
+                  ],
+                ])
+                : Rx.Observable.from([
+                  [
+                    {update: {_index: res._index, _type: res._type, _id: res._id}},
+                    {doc: res._target, doc_as_upsert: upsert},
+                  ]
+                ])
+            )
+            : Rx.Observable.empty()
+        )
+  );
+
+module.exports.bulkCreate = ({rxClient, bulkSize = 100, test = true, concurrency, retries, delay}) => (observable) =>
+  bulk({rxClient, bulkSize, test, concurrency, retries, delay})(
+    observable,
+    res =>
+      res._target
+        ? Rx.Observable.from([
+          {create: {_index: res._index, _type: res._type, _id: res._id}},
+          res._target
+        ])
+        : Rx.Observable.empty()
+  );
